571a4b0c7394fb8ae714bf370d67d431976e4503
bpkcongli <andrian8367@gmail.com>
2024-11-09 21:57:36 +0700

    feat: implement swipe-to-refresh on product list screen

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -3,15 +3,41 @@ import './user/user_auth_screen.dart';
 import '../controllers/login_controller.dart';
 import '../controllers/product_controller.dart';
 import '../exceptions/app_exception.dart';
+import '../models/product.dart';
 import '../screens/product_form_screen.dart';
 
-class ProductListScreen extends StatelessWidget {
+class ProductListScreen extends StatefulWidget {
   final String username;
+
+  const ProductListScreen({ super.key, required this.username });
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
   final ProductController controller = ProductController();
   final LoginController loginController = LoginController();
   final String currency = 'Rp';
+  late Future<List<Product>> _products;
 
-  ProductListScreen({ super.key, required this.username });
+  @override
+  void initState() {
+    super.initState();
+    _loadProducts();
+  }
+
+  void _loadProducts() {
+    _products = controller.getAllProducts();
+  }
+
+  Future<void> _refreshProduct() async {
+    setState(() {
+      _loadProducts();
+    });
+
+    await _products;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,105 +65,108 @@ class ProductListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Selamat datang, $username!',
-                  style: const TextStyle(
-                    fontSize: 16,
+      body: RefreshIndicator(
+        onRefresh: _refreshProduct,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Selamat datang, ${widget.username}!',
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                      return ProductFormScreen(productController: controller);
-                    }));
-                  },
-                  child: const Text('Tambah Produk'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Expanded(
-              child: FutureBuilder(
-                future: controller.getAllProducts(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasError) {
-                    return const Center(
-                      child: Text('Gagal mendapatkan daftar produk.'),
-                    );
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(
-                      child: Text('Belum ada produk yang ditambahkan.'),
-                    );
-                  } else {
-                    final products = snapshot.data!;
-
-                    return ListView.builder(
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        final product = products[index];
-
-                        return ListTile(
-                          title: Text(product.name),
-                          subtitle: Text('$currency ${product.price.truncate().toString()}'),
-                          trailing: PopupMenuButton<String>(
-                            itemBuilder: (context) {
-                              return {'Edit', 'Delete'}.map((option) {
-                                return PopupMenuItem<String>(
-                                  value: option,
-                                  child: Text(option),
-                                );
-                              }).toList();
-                            },
-                            onSelected: (String option) {
-                              switch (option) {
-                                case 'Edit': {
-                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                                    return ProductFormScreen(productController: controller, productId: product.id);
-                                  }));
-                                }
-                                case 'Delete': {
-                                  final isMounted = context.mounted;
-
-                                  controller.deleteProduct(product.id)
-                                    .then((isDeleteProductSucceed) {
-                                      if (isDeleteProductSucceed) {
-                                        if (isMounted) {
-                                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                                            content: Text('Produk berhasil dihapus.'),
-                                          ));
-                                        }
-                                      }
-                                    }).onError((AppException e, _) {
-                                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                        content: Text(e.message),
-                                      ));
-                                    });
-                                }
-                              }
-                            },
-                          ),
-                          onTap: () {},
-                        );
-                      },
-                    );
-                  }
-                },
+                  const SizedBox(height: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                        return ProductFormScreen(productController: controller);
+                      }));
+                    },
+                    child: const Text('Tambah Produk'),
+                  ),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(height: 8),
+              Expanded(
+                child: FutureBuilder(
+                  future: _products,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return const Center(
+                        child: Text('Gagal mendapatkan daftar produk.'),
+                      );
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(
+                        child: Text('Belum ada produk yang ditambahkan.'),
+                      );
+                    } else {
+                      final products = snapshot.data!;
+        
+                      return ListView.builder(
+                        itemCount: products.length,
+                        itemBuilder: (context, index) {
+                          final product = products[index];
+        
+                          return ListTile(
+                            title: Text(product.name),
+                            subtitle: Text('$currency ${product.price.truncate().toString()}'),
+                            trailing: PopupMenuButton<String>(
+                              itemBuilder: (context) {
+                                return {'Edit', 'Delete'}.map((option) {
+                                  return PopupMenuItem<String>(
+                                    value: option,
+                                    child: Text(option),
+                                  );
+                                }).toList();
+                              },
+                              onSelected: (String option) {
+                                switch (option) {
+                                  case 'Edit': {
+                                    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                                      return ProductFormScreen(productController: controller, productId: product.id);
+                                    }));
+                                  }
+                                  case 'Delete': {
+                                    final isMounted = context.mounted;
+        
+                                    controller.deleteProduct(product.id)
+                                      .then((isDeleteProductSucceed) {
+                                        if (isDeleteProductSucceed) {
+                                          if (isMounted) {
+                                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                              content: Text('Produk berhasil dihapus.'),
+                                            ));
+                                          }
+                                        }
+                                      }).onError((AppException e, _) {
+                                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                          content: Text(e.message),
+                                        ));
+                                      });
+                                  }
+                                }
+                              },
+                            ),
+                            onTap: () {},
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
