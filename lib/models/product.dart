21240d0d91d83938d6738fce6b185f4d03c5c184
bpkcongli <andrian8367@gmail.com>
2024-11-09 23:21:24 +0700

    refactor: remove unnecessary method on product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,5 +1,3 @@
-import 'package:uuid/uuid.dart';
-
 class Product {
   String id;
   String code;
@@ -10,19 +8,5 @@ class Product {
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(json['id'], json['code'], json['name'], double.parse(json['price']));
-  }
-
-  factory Product.build(String name, String description, double price) {
-    const uuid = Uuid();
-    
-    return Product(uuid.v4(), name, description, price);
-  }
-
-  Product edit(String name, String description, double price) {
-    name = name;
-    description = description;
-    price = price;
-
-    return this;
   }
 }
